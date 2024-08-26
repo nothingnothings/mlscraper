ad81b0d8549afd19cd7ffa33afca296d2f857614
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-08-26 17:50:27 -0300

    initial working version

--- mlscraper/spiders/scraper.py
+++ mlscraper/spiders/scraper.py
@@ -4,6 +4,7 @@ import pymysql
 from pymysql.err import OperationalError, ProgrammingError
 
 
+
 USER_AGENTS = [
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5672.126 Safari/537.36",
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:113.0) Gecko/20100101 Firefox/113.0",
@@ -21,7 +22,7 @@ USER_AGENTS = [
         "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:113.0) Gecko/20100101 Firefox/113.0"
     ]
 
-class MlScraperSpider(scrapy.Spider):
+class MLScraperSpider(scrapy.Spider):
     name = 'mlscraper'
 
     # Database Parameters
